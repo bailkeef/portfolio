9ea305582ab09d904bc7aedbd6b7cd0ffdca8989
ramilamparo <ramilamparo@gmail.com>
2018-12-16 02:33:56 +0400

    Move to Sassy CSS styling.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./style.css";
+import "./sass/main.scss";
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 

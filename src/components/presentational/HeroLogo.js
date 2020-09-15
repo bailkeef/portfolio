86e55e3733113851b4692df7b803ab5d4432e3d1
Bailey Keefer <bnk423@gmail.com>
2020-09-14 21:13:51 -0400

    finishehd links for linkedin and github

--- src/components/presentational/HeroLogo.js
+++ src/components/presentational/HeroLogo.js
@@ -8,11 +8,22 @@ class HeroLogo extends Component {
 	};
 	static defaultProps = {
 		src: "/img/favicon.png",
+		linkedin: "https://www.linkedin.com/in/baileykeefer/",
+		linkedin_img: "/img/linkedin-white.png",
+		github: "https://github.com/",
+		github_img: "/img/github-logo-white.png",
+
 	};
 	render() {
 		return (
 			<div className="hero__logo-box">
-				<img src={this.props.src} className="hero__logo" alt="Logo"/>
+				{/* <img src={this.props.src} className="hero__logo" alt="Logo"/> */}
+				<a href={this.props.linkedin}>
+					<img src={this.props.linkedin_img} className="hero__logo hero__logo--1" alt="Logo" />
+				</a>
+				<a href={this.props.github}>
+					<img src={this.props.github_img} className="hero__logo hero__logo--2" alt="Logo" />
+				</a>
 			</div>
 		);
 	}

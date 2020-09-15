5b03141dcd9d259c5a723a69f2441f1a78354c23
Bailey Keefer <bnk423@gmail.com>
2020-09-14 22:18:49 -0400

    Updates

---
+++ precache-manifest.9e624f00da953732e859be5bebef059b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b0daa5d0341e5ec3839",
+    "url": "/portfolio/static/css/main.b8523b45.chunk.css"
+  },
+  {
+    "revision": "2b0daa5d0341e5ec3839",
+    "url": "/portfolio/static/js/main.2b0daa5d.chunk.js"
+  },
+  {
+    "revision": "7258aa498efb1b5b3320",
+    "url": "/portfolio/static/js/1.7258aa49.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8d531c020a8b39c3dd44fe16e657cca3",
+    "url": "/portfolio/index.html"
+  }
+];

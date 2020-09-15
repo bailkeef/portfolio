3555b3f1e65b0af96023ab6313819dedd4a58a15
Bailey Keefer <bnk423@gmail.com>
2020-09-15 09:12:39 -0400

    Updates

---
+++ precache-manifest.6e832f87acaccf3f6b8027822c7f0be9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "434ad8d83fc9523d1745",
+    "url": "/portfolio/static/css/main.8102ce2c.chunk.css"
+  },
+  {
+    "revision": "434ad8d83fc9523d1745",
+    "url": "/portfolio/static/js/main.434ad8d8.chunk.js"
+  },
+  {
+    "revision": "7258aa498efb1b5b3320",
+    "url": "/portfolio/static/js/1.7258aa49.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4652bacc0a093e1b1fedf0dc2135d019",
+    "url": "/portfolio/static/media/home-1.4652bacc.jpg"
+  },
+  {
+    "revision": "8ebfc8b87583968f76a84855eda9e1ad",
+    "url": "/portfolio/static/media/code-1.8ebfc8b8.jpg"
+  },
+  {
+    "revision": "5639d45bb1b990408389df5fe4ac36db",
+    "url": "/portfolio/static/media/rei-1.5639d45b.jpg"
+  },
+  {
+    "revision": "62ea26f40a9b6692affee3139bd7f007",
+    "url": "/portfolio/static/media/puzzle-1.62ea26f4.jpeg"
+  },
+  {
+    "revision": "7587ee6a9bdde78271467b3ecb9a9dd0",
+    "url": "/portfolio/static/media/cocktails-1.7587ee6a.jpg"
+  },
+  {
+    "revision": "9ad4eb9cb51c735fc21b1f9116f528f7",
+    "url": "/portfolio/static/media/money-1.9ad4eb9c.jpeg"
+  },
+  {
+    "revision": "b1237b4d1a3c8e347c20d44778f89d19",
+    "url": "/portfolio/static/media/coming-soon.b1237b4d.gif"
+  },
+  {
+    "revision": "1d0015a22697368a04824441d6a8cfe2",
+    "url": "/portfolio/static/media/not-available.1d0015a2.gif"
+  },
+  {
+    "revision": "bd26f2c20a34133421024ae33d3fabfb",
+    "url": "/portfolio/static/media/mazuma-1.bd26f2c2.gif"
+  },
+  {
+    "revision": "04b093a03cdd9cc6b387137ec9d94382",
+    "url": "/portfolio/static/media/tokyo-1.04b093a0.gif"
+  },
+  {
+    "revision": "1a616513b4301ccbba8c5aaa69e8e072",
+    "url": "/portfolio/static/media/coding-1.1a616513.gif"
+  },
+  {
+    "revision": "08f271232b77167e1ad0d92505fa563f",
+    "url": "/portfolio/static/media/cocktails.08f27123.gif"
+  },
+  {
+    "revision": "514983982786eeb6c346506284206b19",
+    "url": "/portfolio/static/media/puzzle-party.51498398.gif"
+  },
+  {
+    "revision": "39dd3c366c72a0ab91a395b1bce49d92",
+    "url": "/portfolio/static/media/bailey-nicole-keefer.39dd3c36.gif"
+  },
+  {
+    "revision": "952db15021f729615d58a9e6d69f885e",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2fc4fe14e58a6b01f3c40da9218164f5.js"
+  "/portfolio/precache-manifest.6e832f87acaccf3f6b8027822c7f0be9.js"
 );
 
 workbox.clientsClaim();
